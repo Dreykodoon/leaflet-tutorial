647207e9d9eec7b3a0f92f4fa69e6f0642a1b9cf
Mihai Cioban <mcioban@axway.com>
2018-07-20 11:32:25 +0300

    Map centers on your location when you click on it.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React, {Component} from 'react';
+import React, {createRef, Component} from 'react';
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import './App.css';
 
 class App extends Component {
 	state = {
-		lat:  51.505,
-		lng:  -0.09,
+		latlng: {
+			lat: 44.44,
+			lng: 26.1,
+		},
 		zoom: 13,
+		hasLocation: false,
+	};
+
+	mapRef = createRef();
+
+	handleClick = () => {
+		this.mapRef.current.leafletElement.locate();
+	};
+
+	handleLocationFound = (e) => {
+		this.setState({
+			hasLocation: true,
+			latlng: e.latlng,
+		});
 	};
 
 	render() {
-		const position = [this.state.lat, this.state.lng];
+		const {latlng, zoom} = this.state;
+
+		const marker = this.state.hasLocation ? (
+			<Marker position={ this.state.latlng }>
+				<Popup>
+					<span>You are here</span>
+				</Popup>
+			</Marker>
+		) : null;
+
 		return (
-			<Map center={ position } zoom={ this.state.zoom }>
+			<Map center={ latlng } zoom={ zoom } onClick={ this.handleClick }
+			onLocationFound={ this.handleLocationFound } ref={this.mapRef}>
 				<TileLayer
 					attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-				<Marker position={ position }>
-					<Popup>
-						A pretty CSS3 popup. <br/> Easily customizable.
-					</Popup>
-				</Marker>
+				{marker}
 			</Map>
 		);
 	}
